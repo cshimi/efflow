89423eefb68bbf7ef95dfba7670a5f4177a573a6
Christiana Shimitra <christianashimitra@Christianas-MacBook-Pro.local>
2023-10-07 14:10:18 +0200

    removing the \ufeff

--- analysis_project/analysis/views.py
+++ analysis_project/analysis/views.py
@@ -11,11 +11,11 @@ def upload_file(request):
             file_instance.save()
 
             # Read and process the CSV file
-            with open(file_instance.file.path, mode='r') as csvfile:
+            with open(file_instance.file.path, mode='r', encoding='utf-8-sig') as csvfile:
                 csv_reader = csv.reader(csvfile)
                 data = list(csv_reader)
 
-            print(data)
+            #'print(data)
             # Extract summary information
             summary = {}
             for row in data[:5]:
@@ -23,7 +23,10 @@ def upload_file(request):
                 key = key.strip().replace(':', '')
                 summary[key.strip()] = value.strip().strip('"')
 
-            print(summary)
+            #print(summary)
+            #{'Date from': '04.10.2021', 'Date to': '04.10.2023', 'Entry type': 'All',
+             #'Account': 'CH5709000000158028028', 'Currency': 'CHF'}
+
             #for key, value in summary.items():
             #    print(f"Key: {key}, Value: {value}")
 
